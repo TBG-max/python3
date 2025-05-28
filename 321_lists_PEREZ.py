2bae7ac55b12090468a054d9d2dae46a2a1d869d
TBG-max <cristian.perez@stu.vertexacademies.org>
2025-05-28 18:14:09 +0000

    finished

--- 321_lists_PEREZ.py
+++ 321_lists_PEREZ.py
@@ -1,1 +1,10 @@
 print("hello world")
+food = ["Burger", "Shake", "Fries"]
+
+
+print(food[0], "is located at index 0.")
+if("sundae" in food):
+    print("you can have a sundae")
+else:
+    print("we dont have that. do you want something else?") 
+ 
